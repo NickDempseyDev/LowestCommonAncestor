a048d2f7ca6053e7cb211899f98031b9c9000d5f
NickDempseyDev <nickdempseydev@gmail.com>
2021-10-31 16:07:46 +0000

    created new branch for DAG implementation and created the tests

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -1,65 +1,6 @@
-import java.util.ArrayList;
+
 
 public class LowestCommonAncestor {
 	
-	BinaryTree<Integer, Integer> bst;
-	
-	public LowestCommonAncestor(int[] nums) {
-		bst = new BinaryTree<Integer, Integer>();
-		buildTree(nums, bst);
-	}
-	
-	public LowestCommonAncestor(String fileName) {
-		bst = new BinaryTree<Integer, Integer>();
-		buildTree(fileName, bst);
-	}
-	
-	
-	public Integer findLCA(Integer no1, Integer no2) {
-		
-		// make sure the inputs are not null
-		if (no1 == null || no2 == null) {
-			bst.path1 = null;
-			bst.path2 = null;
-			return null;
-		}
-		
-		bst.findPaths(no1, no2);
-		// return null if it can't find a path 
-		if (bst.path1 == null || bst.path2 == null) return null;
-		
-		int i;
-	    for (i = 0; i < bst.path1.size() && i < bst.path2.size() ; i++)
-	    {
-	    	if (bst.path1.get(i) != bst.path2.get(i))
-	    	{
-	    		break;
-	    	}
-	    }
-	    return bst.path1.get(i - 1);
-	}
-	
-	
-	public void buildTree(String fileName, BinaryTree<Integer, Integer> bst) {
-		FileLoader loader = new FileLoader(fileName);
-		ArrayList<String> inputs = loader.readFile();
-		for(int i = 0; i < inputs.size(); i++)
-		{
-			bst.put(Integer.parseInt(inputs.get(i)), Integer.parseInt(inputs.get(i)));
-		}
-	}
-	
-	public static void buildTree(int[] nums, BinaryTree<Integer, Integer> bst) {
-		for(int i = 0; i < nums.length; i++)
-		{
-			bst.put(nums[i], nums[i]);
-		}
-	}
-
-	public static void main(String[] args) {
-	
-		LowestCommonAncestor lca = new LowestCommonAncestor("D:\\dev\\College\\Year 3\\Java\\LowestCommonAncestor\\src\\inputs.txt");
-		lca.findLCA(1,4);
-	}
 
 }
--- src/TestLowestCommonAncestor.java
+++ src/TestLowestCommonAncestor.java
@@ -1,37 +1,77 @@
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
 
 @RunWith(JUnit4.class)
 public class TestLowestCommonAncestor {
 
 	@Test
 	// test the constructor and check to see if the tree is generated as expected
-	public void testConstructor1() {
-		int arr[] = {10,5,20,3,7,15,25,1,4,6,8,12,17,22,27};
-		LowestCommonAncestor lca = new LowestCommonAncestor(arr);
-		assertEquals("","((((()1())3(()4()))5((()6())7(()8())))10(((()12())15(()17()))20((()22())25(()27()))))",lca.bst.printKeysInOrder());
+	public void testConstructorDAG() {
+		 DAG graph = new DAG(9);
+		 assertEquals("checking size of graph", 9, graph.V);
+		 assertEquals("checking size of graph", 9, graph.adj.size());
+		 
+		 graph = new DAG(0);
+		 assertEquals("checking size of empty graph", 0, graph.V);
+		 assertEquals("checking size of empty graph", 0, graph.adj.size());
 	}
 	
 	@Test
-	// test the constructor and check to see if the tree is generated as expected
-	public void testConstructor2() {
-		String file = "D:\\dev\\College\\Year 3\\Java\\LowestCommonAncestor\\src\\inputs.txt";
-		LowestCommonAncestor lca = new LowestCommonAncestor(file);
-		assertEquals("","((((()1())3(()4()))5((()6())7(()8())))10(((()12())15(()17()))20((()22())25(()27()))))",lca.bst.printKeysInOrder());
+	//test to make sure the graph adds the edges correctly
+	public void testAddingEdges() {
+		DAG graph = new DAG(9);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 2);
+		graph.addEdge(1, 4);
+		graph.addEdge(1, 6);
+		graph.addEdge(2, 4);
+		graph.addEdge(2, 6);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 6);
+		graph.addEdge(6, 5);
+		graph.addEdge(6, 7);
+		graph.addEdge(7, 8);
+		assertEquals("checking if edge 0 -> 1 exists", Integer.valueOf(1), Integer.valueOf(graph.adj.get(0).get(0).v));
+		assertEquals("checking if edge 1 -> 4 exists", Integer.valueOf(4), Integer.valueOf(graph.adj.get(1).get(0).v));
+		assertEquals("checking if edge 2 -> 6 exists", Integer.valueOf(6), Integer.valueOf(graph.adj.get(2).get(1).v));
+		assertEquals("checking if edge 7 -> 8 exists", Integer.valueOf(8), Integer.valueOf(graph.adj.get(7).get(0).v));
 	}
 	
 	@Test
 	public void testLCA() {
-		int arr[] = {10,5,20,3,7,15,25,1,4,6,8,12,17,22,27};
-		LowestCommonAncestor lca = new LowestCommonAncestor(arr);
-		assertEquals("Find LCA of 6 and 8 (expect 7)",(Integer)7,lca.findLCA(6, 8));
-		assertEquals("Find LCA of 27 and 1 (expect 10)",(Integer)10,lca.findLCA(27,1));
-		assertEquals("Find LCA of 8 and 1 (expect 5)",(Integer)5,lca.findLCA(1, 8));
-		assertEquals("Find LCA of 2121 and 8 (expect null)",null,lca.findLCA(2121, 8));
-		assertEquals("Find LCA of null and null (expect null)",null,lca.findLCA(null, null));
+		DAG graph = new DAG(9);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 2);
+		graph.addEdge(1, 4);
+		graph.addEdge(1, 6);
+		graph.addEdge(2, 4);
+		graph.addEdge(2, 6);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 6);
+		graph.addEdge(6, 5);
+		graph.addEdge(6, 7);
+		graph.addEdge(7, 8);
+		LowestCommonAncestor lca = new LowestCommonAncestor(graph);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		assertArrayEquals("checking the LCA of 4 and 7", list.toArray(), lca.lowestCommonAncestor(4, 7).toArray());
+		
+		list.clear();
+		list.add(0);
+		assertArrayEquals("checking the LCA of 1 and 3", list.toArray(), lca.lowestCommonAncestor(1, 3).toArray());
+		
+		list.clear();
+		list.add(6);
+		assertArrayEquals("checking the LCA of 5 and 8", list.toArray(), lca.lowestCommonAncestor(5, 8).toArray());
 	}
+
 
 }
